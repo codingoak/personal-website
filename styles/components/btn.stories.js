f2760989c0e92096c4faa8ca00f7b640395805f0
Daniel Eicher <d.eicher@mailbox.org>
2022-01-20 08:43:36 +0100

    Add a buttons all file to storybook

--- styles/components/btn.stories.js
+++ styles/components/btn.stories.js
@@ -1,8 +1,11 @@
 import "./btn.css";
+import btns from "./btn-all.html";
 
 export default {
   title: "Component/Button",
 };
 
+export const All = () => btns;
 export const Regular = () => `<button class="btn">click me!</button>`;
+export const Round = () => `<button class="btn btn-round">click me!</button>`;
 export const Fancy = () => `<button class="btn btn-fancy">click me!</button>`;
